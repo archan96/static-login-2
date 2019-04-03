80eced6ac57dafb7b99ee0040f3e6ac7a9b3e48b
archan96 <archan.ray2011@gmail.com>
2019-04-03 21:43:59 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      logIn: false,
+      name: "",
+      email: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+  emailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleLogin = () => {
+    this.state.name.length > 1 &&
+      this.state.email.length > 1 &&
+      this.setState({ logIn: true });
+
+    this.state.name.length <= 1 && window.alert("type a larger name!");
+    this.state.name.email <= 1 && window.alert("type a larger email!");
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className="container">
+          {!this.state.logIn && (
+            <div className="LoginSec">
+              <input
+                type="text"
+                placeholder="Enter your name"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+              <input
+                type="text"
+                placeholder="Enter your Email ID"
+                onChange={this.emailChange}
+                value={this.state.email}
+              />
+
+              <div className="btn" onClick={this.handleLogin}>
+                <p>LOG IN</p>
+              </div>
+            </div>
+          )}
+
+          {this.state.logIn && (
+            <div className="LoginSec log">
+              <p>{this.state.name}</p>
+              <p>{this.state.email}</p>
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
